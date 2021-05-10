597bdcdc124f1e86c6201fee9a6f05c1f1fda5e1
Fabian Lippold <fabianlippold1184@gmail.com>
2021-05-10 23:20:37 +0200

    fetz

---
+++ bot.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import api
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix="!")
+
+@bot.command(name="stonks", description="show leaderboard")
+async def stonks(ctx, *args):
+    await api.main(ctx, args)
+
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send("ALLAH KAPUTTPUTT!!!:\n"+str(error))
+
+@bot.event
+async def on_ready():
+    print("READY")
+
+bot.run(TOKEN)
